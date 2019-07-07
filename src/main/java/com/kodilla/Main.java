566add5926d8c5f1d968354f9f382b07a2f94e8f
TheRitual <dev@theritual.eu>
2019-07-07 22:35:44 +0200

    Updated Gradle for jar files

--- src/main/java/com/kodilla/Main.java
+++ src/main/java/com/kodilla/Main.java
@@ -1,7 +1,10 @@
 package com.kodilla;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test!");
+        Arrays.stream(args).forEach(System.out::println);
     }
 }
